b52a2023b88f9af987f562362b33290675a76e8e
aRTIKa-afk <artiiv240606@mail.ru>
2025-05-27 01:30:40 +0300

    make pattern to predistions

--- sudoku.cpp
+++ sudoku.cpp
@@ -8,27 +8,35 @@
 // Artem Ivanov, MIPT, Phystech-School AMCS.
 // Feb 2025
 
-#include <chrono>    //working time
-#include <fstream>   //csv output
-#include <iostream>  //std lib
-#include <vector>    //vector lib
+#include <chrono>   //working time
+#include <cstdint>
+#include <cstdlib>
+#include <fstream>  //csv output
+#include <iostream> //std lib
+#include <mutex>
+#include <stack>
+#include <utility>
+#include <vector>   //vector lib
 
-const int cSheetSize = 9;  // size of sheet
 
-class Cells {  // class for draw cell
- private:
+const int cSheetSize = 9; // size of sheet
+
+
+class Cells { // class for draw cell
+private:
   std::vector<bool> IsPosible_ = std::vector<bool>(cSheetSize, true);
   int count_ = cSheetSize;
   bool origin_ = false;
 
- public:
+public:
   bool IsLast() const { return count_ == 1; }
+  bool IsCountZero() const { return count_ == 0; }
   bool IsOrigin() const { return origin_; }
   bool IsPos(int num) const { return IsPosible_[num - 1]; }
   void MakeOrigin() { origin_ = true; }
   void DecreaseCount() { --count_; }
   void ChangePosibility(int num) { IsPosible_[num - 1] = false; }
-  int LastNumber() {
+  int LastNumber() const {
     for (int i = 0; i < cSheetSize; ++i) {
       if (IsPosible_[i]) {
         return i + 1;
@@ -38,20 +46,38 @@ class Cells {  // class for draw cell
   }
 };
 
-class DrawField {  // class for draw field
- private:
+class DrawField { // class for draw field
+private:
   int count_free_cells_ = cSheetSize * cSheetSize;
   std::vector<std::vector<Cells>> draw_field_ = std::vector<std::vector<Cells>>(
       cSheetSize, std::vector<Cells>(cSheetSize));
 
- public:
+public:
+  int Data(int i, int j) const {return draw_field_[i][j].LastNumber();}
   void MakeOrigin(int i, int j) { draw_field_[i][j].MakeOrigin(); }
+  void ChangePosibility(int i, int j, int num) { draw_field_[i][j].ChangePosibility(num); }
   void DecreaseCountFree() { --count_free_cells_; }
   void IncreaseCountFree() { ++count_free_cells_; }
   int CountFreeCells() const { return count_free_cells_; }
-
-  bool Filling(std::vector<std::vector<int>>& field) {
+  std::pair<int8_t, int8_t> Predict(){
+    for(int i = 0; i<cSheetSize; ++i){
+      for(int j = 0; j<cSheetSize; ++j){
+        if(not draw_field_[i][j].IsOrigin()){
+          while(not draw_field_[i][j].IsLast()){
+            draw_field_[i][j].ChangePosibility(draw_field_[i][j].LastNumber());
+            draw_field_[i][j].DecreaseCount();
+          }
+          std::cout<<draw_field_[i][j].LastNumber();
+          MakeOrigin(i, j);
+          DecreaseCountFree();
+          return std::pair<int8_t, int8_t>(i,j);
+        }
+      }
+    }
+  }
+  std::pair<bool, bool> Filling(std::vector<std::vector<int>> &field) {
     bool updated = false;
+    bool is_possible = true;
     for (int i = 0; i < cSheetSize; ++i) {
       for (int j = 0; j < cSheetSize; ++j) {
         if (not draw_field_[i][j].IsOrigin()) {
@@ -81,63 +107,84 @@ class DrawField {  // class for draw field
 
     for (int i = 0; i < cSheetSize; ++i) {
       for (int j = 0; j < cSheetSize; ++j) {
-        if (not draw_field_[i][j].IsOrigin() && draw_field_[i][j].IsLast()) {
-          draw_field_[i][j].MakeOrigin();
-          field[i][j] = draw_field_[i][j].LastNumber();
-          --count_free_cells_;
-          updated = true;
+        if (not draw_field_[i][j].IsOrigin()) {
+          if (draw_field_[i][j].IsLast()) {
+            draw_field_[i][j].MakeOrigin();
+            field[i][j] = draw_field_[i][j].LastNumber();
+            --count_free_cells_;
+            updated = true;
+          } else if (draw_field_[i][j].IsCountZero()) {
+            is_possible = false;
+            break;
+          }
         }
       }
     }
-    return updated;
+    return std::pair<bool, bool>(updated, is_possible);
   }
 };
-
-void Printing(std::vector<std::vector<int>>& field,
-              std::vector<std::vector<bool>>& is_input, bool to_console = true,
+std::stack<DrawField> call_stack;
+std::stack<std::pair<int8_t, int8_t>> coordinations_stack;
+void Printing(std::vector<std::vector<int>> &field,
+              std::vector<std::vector<bool>> &is_input, bool to_console = true,
               bool to_csv = true,
-              bool highlight = true);  // function to get output
+              bool highlight = true); // function to get output
 
-void Input(std::vector<std::vector<int>>& field,
-           std::vector<std::vector<bool>>& is_input,
-           DrawField& draw_field);  // function which get input
+void Input(std::vector<std::vector<int>> &field,
+           std::vector<std::vector<bool>> &is_input,
+           DrawField &draw_field); // function which get input
 
 int main() {
   std::vector<std::vector<int>> field = std::vector<std::vector<int>>(
-      cSheetSize, std::vector<int>(cSheetSize, 0));  // main field
+      cSheetSize, std::vector<int>(cSheetSize, 0)); // main field
   std::vector<std::vector<bool>> is_input = std::vector<std::vector<bool>>(
       cSheetSize,
-      std::vector<bool>(cSheetSize, false));  // field which store start field
+      std::vector<bool>(cSheetSize, false)); // field which store start field
 
-  DrawField draw_field;  // draw field
+  DrawField draw_field; // draw field
 
   Input(field, is_input, draw_field);
 
   std::cout << "processing...\n";
 
-  auto start = std::chrono::high_resolution_clock::now();  // start timepoint
+  auto start = std::chrono::high_resolution_clock::now(); // start timepoint
 
   while (draw_field.CountFreeCells() != 0) {
-    if (draw_field.Filling(field)) {
-      continue;  // recursively place one-possible digit
+    std::pair<bool, bool> out = draw_field.Filling(field);
+    if (out.first and out.second) {
+      std::cout<<1;
+      continue; // recursively place one-possible digit
     }
-    // Will be soon
+    else if(not out.second and draw_field.CountFreeCells() != 0){
+      std::cout<<2;
+      call_stack.top().ChangePosibility(coordinations_stack.top().first, coordinations_stack.top().first, draw_field.Data(coordinations_stack.top().first, coordinations_stack.top().second));
+      draw_field = call_stack.top();
+      call_stack.pop();
+      coordinations_stack.pop();
+    }
+    else if(not out.first and draw_field.CountFreeCells() != 0){
+      std::cout<<3<<"\n";
+      call_stack.push(draw_field);
+      coordinations_stack.push(draw_field.Predict());
+    }
+    
   }
 
-  auto end = std::chrono::high_resolution_clock::now();  // end timepoint
+  auto end = std::chrono::high_resolution_clock::now(); // end timepoint
 
   double mseconds =
       std::chrono::duration_cast<std::chrono::milliseconds>(end - start)
-          .count();  // calculate time of proccessing
+          .count(); // calculate time of proccessing
 
   std::cout << "done!\n"
             << "process done in " << mseconds << "ms\n";
 
-  Printing(field, is_input, true, false, true);  // printing
+  Printing(field, is_input, true, true, false); // printing
+  
 }
 
-void Printing(std::vector<std::vector<int>>& field,
-              std::vector<std::vector<bool>>& is_input, bool to_console,
+void Printing(std::vector<std::vector<int>> &field,
+              std::vector<std::vector<bool>> &is_input, bool to_console,
               bool to_csv, bool highlight) {
   std::ofstream out_csv;
   if (to_csv) {
@@ -184,8 +231,8 @@ void Printing(std::vector<std::vector<int>>& field,
   }
 }
 
-void Input(std::vector<std::vector<int>>& field,
-           std::vector<std::vector<bool>>& is_input, DrawField& draw_field) {
+void Input(std::vector<std::vector<int>> &field,
+           std::vector<std::vector<bool>> &is_input, DrawField &draw_field) {
   for (int i = 0; i < cSheetSize; ++i) {
     for (int j = 0; j < cSheetSize; ++j) {
       int cur_num = 0;
